024db13435ef7046128039bd9d5d82ffed40a309
Eduardo P.P. Ferreira <ppfeduardo@gmail.com>
2023-05-30 18:26:09 -0300

    Changed A LOT. The code was basically rewritten to use bot.command

--- bot.py
+++ bot.py
@@ -1,14 +1,9 @@
 import os
 import discord
+from discord.ext import commands
 import responses
 from dotenv import load_dotenv
 
-async def send_message(message, user_message, is_private):
-    try:
-        response = responses.handle_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
-    except Exception as e:
-        print(e)
 
 def run_discord_bot():
     load_dotenv()
@@ -16,27 +11,58 @@ def run_discord_bot():
 
     intents = discord.Intents.default()
     intents.message_content = True
-    client = discord.Client(intents=intents)
+    
+    bot = commands.Bot(command_prefix='!', intents=intents)
 
-    @client.event
-    async def on_message(message):
-        if message.author == client.user: # client.user == bot. Não queremos que o bot responda a si mesmo
-            return # Impede interação infinita entre bot e ele mesmo
-        
-        username =     str(message.author)
-        user_message = str(message.content)
-        channel =      str(message.channel)
-
-        print(f'{username} excreveu: "{user_message}" ({channel})') # Para debbugging
-
-        if user_message[0] == "?":
-            user_message = user_message[1:]
-            await send_message(message, user_message, is_private=True)
-        else:
-            await send_message(message, user_message, is_private=False)
-
-    @client.event
+    @bot.event
     async def on_ready():
-        print(f'{client.user} has connected to Discord! weeeee')
+        print(f'Mahiro está online como {bot.user.name}!')
 
-    client.run(TOKEN)
+    @bot.event
+    async def on_message(message):
+        if message.author == bot.user:
+            return
+        
+        # Process commands defined bellow this event.
+        await bot.process_commands(message)
+        
+        username = str(message.author)
+        user_message = str(message.content)
+        print(f'{username} said: "{user_message}".')  # For debugging
+
+        '''
+        This is a prototype of a more "complex" chat with the bot.
+        The ideia is for the bot to respond only when it's name is mentioned.
+        '''
+        if "mahiro" in user_message.lower():  # checking if the user calls for Mahiro
+            try:
+                if user_message[0] == '?':
+                    user_message = user_message[1:]
+                    response = responses.handle_response(user_message)  # handle_response has only call, for now
+                    await message.author.send(response)
+                else:
+                    response = responses.handle_response(user_message)
+                    await message.channel.send(response)
+            except Exception as e:
+                print(e)
+    
+    @bot.command()
+    async def about(ctx):
+        aboutm = "Mahiro Bot V 1.0\nPor enquanto eu só respondo a comandos específicos e ao meu nome... mas em breve terá mais!\n- Código disponível em:\nhttps://github.com/EdPPF/Mahiro-Bot-Project.git"
+        await ctx.send(aboutm)
+
+    @bot.command()
+    async def coms(ctx):
+        lcoms = ["`!about`", "`!coms`", "`!ajuda`"]
+        await ctx.send(lcoms)
+
+    @bot.command()
+    async def ajuda(ctx):
+        helpm = "Para uma lista de comandos, digite `!coms`. Para receber uma resposta minha no privado, inicie sua mensagem com `?`."
+        await ctx.send(helpm)
+
+    @bot.command()
+    async def image(ctx):  # For testing
+        await ctx.send(file=discord.File('imgs\oopsie.png'))
+
+    bot.run(TOKEN)
--- responses.py
+++ responses.py
@@ -1,44 +1,26 @@
 import json
 import random
 
+# Reading chat.json:
+with open("chat.json", "r") as arquivo:
+    json_chat = arquivo.read()
+# Object from chat.json
+chat = json.loads(json_chat)
+
 def handle_response(message):
-    about = "Mahiro Bot V 1.0\n- Código disponível em:\nhttps://github.com/EdPPF/Mahiro-Bot-Project.git"
-    coms = ["`!abt`", "`!com`", "`!help`", "`!image`"]
-    help = "Para uma lista de comandos, digíte `!com`. Para receber uma resposta minha no privado, inicie sua mensagem com `?`."
-
-    # Reading chat.json:
-    with open("chat.json", "r") as arquivo:
-        json_chat = arquivo.read()
-
-    # Object from chat.json
-    chat = json.loads(json_chat)
-
     usr_msg = message.lower()
 
-    # call
-    call = chat[0]
+    call = chat[0]  # call
     list_len = len(call["response"])
     random_call_item = random.randrange(list_len)
     random_call_resp = call["response"][random_call_item]
-    if (usr_msg in call["user_input"]):
-        return random_call_resp
-    
-    # about
-    if (usr_msg == "!abt"):
-        return about
-    
-    # commands
-    if (usr_msg == "!com"):
-        return coms
-    
-    # help
-    if (usr_msg == "!help"):
-        return help
-    
-    # image
-    image = chat[1]
+    # if (usr_msg in call["user_input"]):  # This is already checked (sorta) on bot.on_message
+    #     return random_call_resp
+    return random_call_resp
+
+def choose_image(): # Only for testing sending an image
+    image = chat[1]  # image
     image_list_len = len(image["response"])
     random_img_item = random.randrange(image_list_len)
     random_image = image["response"][random_img_item]
-    if (usr_msg == "!image"):
-        return f"{random_image}"
+    return f"{random_image}"
